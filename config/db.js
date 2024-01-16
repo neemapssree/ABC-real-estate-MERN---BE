5578a80b761c7784643fba99a681cfdc1faf71d4
neemapssree <145227095+neemapssree@users.noreply.github.com>
2024-01-16 21:32:40 +0400

    Updated mongodblink

--- config/db.js
+++ config/db.js
@@ -5,9 +5,7 @@ const connectDB = async () => {
         // const connection = await mongoose.connect('mongodb://127.0.0.1:27017/realestateapp', {
         //     useNewUrlParser:'true'
         // })  
-        const connection = await mongoose.connect('mongodb+srv://neemapssree123:neemapssree123@cluster0.zcm3etm.mongodb.net/', {
-            useNewUrlParser:'true'
-        })
+        const connection = await mongoose.connect('mongodb+srv://neemapssree123:neemapssree123@cluster0.zcm3etm.mongodb.net/')
         console.log("MongoDB connected");
     } catch(err) {
         console.log(err);
