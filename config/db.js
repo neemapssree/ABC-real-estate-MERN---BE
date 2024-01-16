788b6837228c1fb9e88fd7191e7873fe4aba49d3
neemapssree <145227095+neemapssree@users.noreply.github.com>
2024-01-16 15:56:25 +0400

    Updated files

--- config/db.js
+++ config/db.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try { 
-        const connection = await mongoose.connect('mongodb://127.0.0.1:27017/realestateapp', {
-            useNewUrlParser:'true'
-        })
-        // const connection = await mongoose.connect('mongodb+srv://neemapssree123:neemapssree123@cluster0.zcm3etm.mongodb.net/', {
+        // const connection = await mongoose.connect('mongodb://127.0.0.1:27017/realestateapp', {
         //     useNewUrlParser:'true'
         // })
+        const connection = await mongoose.connect('mongodb+srv://neemapssree123:neemapssree123@cluster0.zcm3etm.mongodb.net/', {
+            useNewUrlParser:'true'
+        })
         console.log("MongoDB connected");
     } catch(err) {
         console.log(err);
