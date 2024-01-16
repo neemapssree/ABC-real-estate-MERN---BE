cd6036d7362ca9cc3ac8fc9af24af8f62d91fa13
neemapssree <145227095+neemapssree@users.noreply.github.com>
2024-01-16 20:34:04 +0400

    Updated files

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
     try { 
         // const connection = await mongoose.connect('mongodb://127.0.0.1:27017/realestateapp', {
         //     useNewUrlParser:'true'
-        // })
+        // })  
         const connection = await mongoose.connect('mongodb+srv://neemapssree123:neemapssree123@cluster0.zcm3etm.mongodb.net/', {
             useNewUrlParser:'true'
         })
