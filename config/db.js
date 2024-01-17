df2bfc956df13e3efede7793420ebc709936794f
neemapssree <145227095+neemapssree@users.noreply.github.com>
2024-01-17 18:33:54 +0400

    Updated dbjs

--- config/db.js
+++ config/db.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try { 
-        // const connection = await mongoose.connect('mongodb://127.0.0.1:27017/realestateapp', {
-        //     useNewUrlParser:'true'
-        // })  
-        const connection = await mongoose.connect('mongodb+srv://neemapssree:neemapssree123@realestate.b60dd2z.mongodb.net/')
+        const connection = await mongoose.connect('mongodb://127.0.0.1:27017/realestateapp', {
+            useNewUrlParser:'true'
+        })  
+        // const connection = await mongoose.connect('mongodb+srv://neemapssree:neemapssree123@realestate.b60dd2z.mongodb.net/')
         console.log("MongoDB connected");
     } catch(err) {
         console.log(err);
